e27e7a9e25b37289f6e8ea1c6caeb144f8b31efc
Steven Lao <lao.a.steven@gmail.com>
2019-06-20 01:57:13 -0400

    Header explanation

--- CryptoTool.js
+++ CryptoTool.js
@@ -1,28 +1,43 @@
-import { createCipher, createDecipher } from 'crypto';
-var algorithm = '';
-var password = '';
+/****************************************************************************************************
+ * CryptoTools.js
+ * Tool for encrypting / decrypting paddlers information
+ ****************************************************************************************************/
 
+const crypto = require('crypto');
+const algorithm = '';
+const password = '';
+
+/****************************************************************************************************
+ * Encrypt
+ ****************************************************************************************************/
 function encrypt(text) {
-  var cipher = createCipher(algorithm, password)
+  var cipher = crypto.createCipher(algorithm, password)
   var crypted = cipher.update(text, 'utf8', 'hex')
   crypted += cipher.final('hex');
+
   return crypted;
 }
 
+/****************************************************************************************************
+ * Decrypt
+ ****************************************************************************************************/
 function decrypt(text) {
-  var decipher = createDecipher(algorithm, password)
+  var decipher = crypto.createDecipher(algorithm, password)
   var dec = decipher.update(text, 'hex', 'utf8')
   dec += decipher.final('utf8');
+
   return dec;
 }
 
-var misc = 
+/****************************************************************************************************
+ * Array of paddler's info
+ ****************************************************************************************************/
+var paddlers = 
 [
-  
+  ''
 ]; 
 
-misc.forEach(function (item) {
-  var enc = encrypt(item);
-  console.log(enc);
-  // console.log(decrypt(enc));
+paddlers.forEach(function (paddler) {
+  console.log(encrypt(paddler));
+  // console.log(decrypt(paddler));
 });
